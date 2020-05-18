ef234c1e17c050f8ac9bc15263671bc6e86e9343
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-18 14:11:59 +0000

    Deploy CEBEP9HUH/OTUS_Homework_4 to github.com/CEBEP9HUH/OTUS_Homework_4.git:gh-pages

--- build/CMakeFiles/print_ip_doc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "html"
-  "CMakeFiles/print_ip_doc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/print_ip_doc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
